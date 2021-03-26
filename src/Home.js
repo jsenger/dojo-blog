c7aa4cfa3c20ec2215d40eded8572382eef8feae
jéssica senger <jessicasbogoni1@gmail.com>
2021-03-26 13:50:24 -0400

    Create blogs

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const Home = () => {
+  const [blogs, setBlogs] = useState([
+    { title: "Hey Gorge", body: "lore ipsum...", author: "Gottmik", id: 1 },
+    { title: "Backwards shoe", body: "lore ipsum...", author: "Rosé", id: 2 },
+    {
+      title: "Icy spicy for u im too pricey",
+      body: "lore ipsum...",
+      author: "Denali",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by: {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
