6c341d702526e9847ee8e9e9e125154517d45115
jéssica senger <jessicasbogoni1@gmail.com>
2021-04-05 10:56:44 -0400

    Create button to delete blogs

--- src/Home.js
+++ src/Home.js
@@ -13,9 +13,14 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" />
+      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
       <BlogList
         blogs={blogs.filter((blog) => blog.author === "Gottmik")}
         title="Mik's blogs"
