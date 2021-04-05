f958dbcab9e769cac498695c0a6f289811a1a0f6
jéssica senger <jessicasbogoni1@gmail.com>
2021-04-05 14:45:03 -0400

    Fetch data from JSON

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "Hey Gorge", body: "lore ipsum...", author: "Gottmik", id: 1 },
-    { title: "Backwards shoe", body: "lore ipsum...", author: "Rosé", id: 2 },
-    {
-      title: "Icy spicy for u im too pricey",
-      body: "lore ipsum...",
-      author: "Denali",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
+  useEffect(() => {
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+      });
+  }, []);
+
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />
-      <BlogList
-        blogs={blogs.filter((blog) => blog.author === "Gottmik")}
-        title="Mik's blogs"
-      />
+      {blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete} />}
     </div>
   );
 };
