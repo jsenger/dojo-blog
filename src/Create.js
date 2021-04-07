7df3906a703dbced5acbd263680a2f9c84644091
jéssica senger <jessicasbogoni1@gmail.com>
2021-04-07 14:03:49 -0400

    Add "onChange" function to Create form

--- src/Create.js
+++ src/Create.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
+
 const Create = () => {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [author, setAuthor] = useState("Gottmik");
+
   return (
     <div className="create">
       <h2>Add a new blog</h2>
       <form>
         <label>Blog title:</label>
-        <input type="text" required />
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog body:</label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog author:</label>
-        <select>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
           <option value="Gottmik">Gottmik</option>
           <option value="Rosé">Rosé</option>
           <option value="Denali">Denali</option>
